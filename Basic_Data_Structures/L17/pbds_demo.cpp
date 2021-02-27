7ac5f90386161a8363575c8716a93e9391476766
Pulkit Chhabra <33263664+pulkitchhabra6@users.noreply.github.com>
2021-02-28 02:09:19 +0530

    Create pbds_demo.cpp

---
+++ Basic_Data_Structures/L17/pbds_demo.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define mt              make_tuple
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define range(a,b)		substr(a,b-a+1)
+#define w(x)            int x; cin>>x; while(x--)
+#define trace(x) 		cerr<<#x<<": "<<x<<" "<<endl;
+#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+int32_t main()
+{
+	FIO;
+	int q; cin >> q;
+	pbds s;
+
+	while (q--)
+	{
+		int tp; cin >> tp;
+
+		if (tp == 1)
+		{
+			int x; cin >> x;
+			s.insert(x);
+		}
+
+		if (tp == 2)
+		{
+			int x; cin >> x;
+			s.erase(x);
+		}
+
+		if (tp == 3)
+		{
+			int l, r; cin >> l >> r;
+			r = s.order_of_key(r + 1);
+			l = s.order_of_key(l);
+
+			cout << r - l << '\n';
+		}
+
+	}
+	return 0;
+}
