275b858e7fe2d2ab68a6fbccc48caa47a9b4e28c
Riya bansal <riyabansal98@gmail.com>
2021-03-24 20:32:33 +0530

    Class 5 notes

--- Basic_Data_Structures_Java/L3/LinkedListDemo.java
+++ Basic_Data_Structures_Java/L3/LinkedListDemo.java
@@ -166,20 +166,20 @@ public class LinkedListDemo {
 
 		return slow.data;
 	}
-	
+
 	public void reorderList() {
-		
+
 		Node slow = head;
 		Node fast = head;
 
-		//1. Find mid of LinkedList
+		// 1. Find mid of LinkedList
 		while (fast != null && fast.next != null) {
 
 			slow = slow.next;
 			fast = fast.next.next;
 		}
 
-		//2. Reverse the second half of LL
+		// 2. Reverse the second half of LL
 		Node prev = null;
 		Node curr = slow;
 		Node next = null;
@@ -192,22 +192,95 @@ public class LinkedListDemo {
 			curr = next;
 		}
 
-		//3. Merge two sorted LinkedList
+		// 3. Merge two sorted LinkedList
 		Node first = head;
 		Node second = prev;
 		Node temp;
-		while(second.next != null) {
-			
+		while (second.next != null) {
+
 			temp = first.next;
 			first.next = second;
-			
 			first = temp;
-			
+
 			temp = second.next;
 			second.next = first;
 			second = temp;
 		}
+
+	}
+
+	public int getIntersection(Node head1, Node head2) {
+
+		Node temp = head1;
+		int count1 = 0;
+		while (temp != null) {
+			count1++;
+			temp = temp.next;
+		}
+
+		temp = head1;
+		int count2 = 0;
+		while (temp != null) {
+			count2++;
+			temp = temp.next;
+		}
+
+		int d;
 		
+		if (count1 > count2) {
+		
+			d = count1 - count2;
+			
+			return intersect(d, head1, head2);
+		} else {
+			d = count2 - count1;
+			return intersect(d, head2, head1);
+		}
+	}
+
+	private int intersect(int d, Node head2, Node head1) {
+		
+		Node curr1 = head1;
+		Node curr2 = head2;
+		
+		for(int i = 0; i < d; i++) {
+			if(curr1 == null) {
+				return -1;
+			}
+			
+			curr1 = curr1.next;
+		}
+		
+		while(curr1 != null && curr2 != null) {
+			
+			if(curr1.data == curr2.data) {
+				return curr1.data;
+			}
+			
+			curr1 = curr1.next;
+			curr2 = curr2.next;
+		}
+		
+		return -1;
+	}
+	
+	public boolean detect_cycle(Node head) {
+		
+		Node slow = head;
+		Node fast = head;
+		
+		while(slow != null && fast != null && fast.next != null) {
+			
+			slow = slow.next;
+			fast = fast.next.next;
+			
+			if(slow == fast) {
+				return true;
+			}
+			
+		}
+		
+		return false;
 	}
 
 }
