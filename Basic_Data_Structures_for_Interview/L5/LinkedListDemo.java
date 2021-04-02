b9a4957fedb60866a8c1bcd25d55e0a5ad2b7cac
Riya bansal <riyabansal98@gmail.com>
2021-04-02 13:54:33 +0530

    Class 6 notes

--- Basic_Data_Structures_for_Interview/L5/LinkedListDemo.java
+++ Basic_Data_Structures_for_Interview/L5/LinkedListDemo.java
@@ -138,22 +138,144 @@ public class LinkedListDemo {
 
 		return rv;
 	}
-	
+
+	// O(n)
+	// Iterative Solution
 	public void reverse() {
-		
+
 		Node prev = null;
 		Node curr = head;
 		Node next = null;
-		
-		while(curr != null) {
-			
+
+		while (curr != null) {
+
 			next = curr.next;
 			curr.next = prev;
 			prev = curr;
 			curr = next;
 		}
-		
+
 		head = prev;
 	}
 
+	public boolean detectCycle(Node head) {
+
+		Node slow = head;
+		Node fast = head;
+
+		while (slow != null && fast != null && fast.next != null) {
+
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+
+		slow = head;
+
+		while (slow != fast) {
+			slow = slow.next;
+			fast = fast.next;
+
+		}
+
+		return false;
+	}
+
+	public int getIntersection(Node head1, Node head2) {
+
+		Node temp = head1;
+		int count1 = 0;
+
+		while (temp != null) {
+			count1++;
+			temp = temp.next;
+		}
+		temp = head2;
+		int count2 = 0;
+		while (temp != null) {
+			temp = temp.next;
+			count2++;
+		}
+		int d;
+
+		if (count1 > count2) {
+
+			d = count1 - count2;
+
+			return intersect(d, head1, head2);
+		} else {
+			d = count2 - count1;
+			return intersect(d, head2, head1);
+		}
+	}
+
+	private int intersect(int d, Node head1, Node head2) {
+
+		Node curr1 = head1;
+		Node curr2 = head2;
+
+		for (int i = 0; i < d; i++) {
+			if (curr1 == null) {
+				return -1;
+			}
+			curr1 = curr1.next;
+		}
+
+		while (curr1 != null && curr2 != null) {
+
+			if (curr1.data == curr2.data) {
+
+				return curr1.data;
+			}
+
+			curr1 = curr1.next;
+			curr2 = curr2.next;
+
+		}
+
+		return -1;
+	}
+
+	public void reorderList() {
+
+		Node slow = head;
+		Node fast = head;
+
+		
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+		}
+
+		Node prev = null;
+		Node curr = slow;
+		Node next = null;
+
+		while (curr != null) {
+
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+
+		// Merge
+		Node first = head;
+		Node second = prev;
+		Node temp;
+
+		while (second.next != null) {
+
+			temp = first.next;
+			first.next = second;
+			first = temp;
+			
+			temp = second.next;
+			second.next = first;
+			second = temp;
+		}
+	}
 }
