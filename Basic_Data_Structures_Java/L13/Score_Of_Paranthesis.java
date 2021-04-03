eb09232b56f55cfb1deaf9940139ee400cb14332
Riya bansal <riyabansal98@gmail.com>
2021-04-03 22:23:54 +0530

    Class 13 notes

--- Basic_Data_Structures_Java/L13/Score_Of_Paranthesis.java
+++ Basic_Data_Structures_Java/L13/Score_Of_Paranthesis.java
@@ -1,10 +1,59 @@
 package Basic_Data_Structures_13;
 
+import java.util.Stack;
+
 public class Score_Of_Paranthesis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		String s1 = "(()(()))";
+		System.out.println(score(s1));
+	}
+
+	//TC - O(n)
+	//SC - O(n)
+	private static int score(String s1) {
+		
+		Stack<String> s = new Stack<>();
+		
+		int ans = 0;
+		
+		for(int i = 0; i < s1.length(); i++) {
+			
+			if(s1.charAt(i) == '(') {
+				s.push("(");
+			}else {
+				
+				if(s.peek() == "(") {
+					s.pop();
+					s.add("1");
+				}else {
+					
+					int c = 0;
+					
+					while(s.peek() != "(") {
+						
+						c += Integer.parseInt(s.peek());
+						s.pop();
+					}
+					
+					s.pop();
+					
+					s.push(String.valueOf(2* c));
+				}
+			}
+			
+			
+		}
+		
+		while(!s.isEmpty()) {
+			
+			ans += Integer.parseInt(s.peek());
+			s.pop();
+		}
+
+		return ans;
 	}
 
 }
