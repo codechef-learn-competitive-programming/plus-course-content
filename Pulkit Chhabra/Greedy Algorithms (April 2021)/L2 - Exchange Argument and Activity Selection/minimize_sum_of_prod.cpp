cb73de76f23a87c11553329fb80a6509934b37a3
Pulkit Chhabra <33263664+pulkitchhabra6@users.noreply.github.com>
2021-04-22 05:51:18 +0530

    Create minimize_sum_of_prod.cpp

---
+++ Pulkit Chhabra/Greedy Algorithms (April 2021)/L2 - Exchange Argument and Activity Selection/minimize_sum_of_prod.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define mt              make_tuple
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define range(a,b)		substr(a,b-a+1)
+#define w(x)            int x; cin>>x; while(x--)
+#define trace(x) 		cerr<<#x<<": "<<x<<" "<<endl;
+#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+int32_t main()
+{
+	FIO;
+	int n; cin >> n;
+
+	vi a(n), b(n);
+
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	for (int i = 0; i < n; ++i)
+		cin >> b[i];
+
+	sort(a.begin(), a.end());
+	sort(b.rbegin(), b.rend());
+
+	int ans = 0;
+
+	for (int i = 0; i < n; ++i)
+		ans += a[i] * b[i];
+
+	cout << ans << '\n';
+	return 0;
+}
